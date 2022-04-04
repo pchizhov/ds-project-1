76fb740a1fbd4a6af9be9ec3649e53f1bc1f5fe4
pchizhov <chizhov.pasha@yandex.ru>
2022-04-04 11:03:55 +0300

    valid algorithm

--- main.py
+++ main.py
@@ -23,10 +23,11 @@ class Process:
 
     def __init__(self, pid: int, port: int, other_ports: set):
         super().__init__()
-        self.pid = f'P{pid}'
+        self.pid = pid
         self.port = port
         self.state = State.DO_NOT_WANT
         self.timestamp = 0
+        self.sent_timestamp = 0
         self.queue = Queue()
         self.terminated = threading.Event()
         self.others = [(LOCALHOST, prt) for prt in other_ports]
@@ -51,53 +52,71 @@ class Process:
     @staticmethod
     def decode_request(data: bytes):
         pid, t = data.decode().strip().split(' ')
-        return pid, int(t)
+        return int(pid), int(t)
 
     @staticmethod
     def ok_response():
         return 'OK'.encode()
+
+    def to_queue(self, pid, t):
+        """
+        In lecture 6 it was not defined what to do when the timestamps match,
+        therefore I added a rule, that in such case a process with lower
+        id wins.
+        """
+        return t > self.sent_timestamp or t == self.sent_timestamp and pid > self.pid
 
     @staticmethod
     def send_to_process(address: tuple, message: str):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(address)
         sock.send(message.encode())
-        result = sock.recv(1024).decode()
+        result = sock.recv(128).decode()
         return result == "OK"
+
+    def broadcast_and_wait(self, message):
+        sent_threads = []
+        for address in self.others:
+            new_thread = threading.Thread(target=self.send_to_process, args=(address, message))
+            sent_threads.append(new_thread)
+            new_thread.start()
+        for thread in sent_threads:
+            thread.join()
+
+    def possess(self):
+        self.state = State.HELD
+        time.sleep(self.tcs_timeout())
+        self.state = State.DO_NOT_WANT
+        while not self.queue.empty():
+            flag = self.queue.get()
+            flag.set()
 
     def apply_for_cs(self):
         while not self.terminated.is_set():
             time.sleep(self.tp_timeout())
-            self.state = State.WANTED
             self.timestamp += 1
-            message = f'{self.pid} {self.timestamp}'
-            for address in self.others:
-                self.send_to_process(address, message)
-            self.state = State.HELD
-            time.sleep(self.tcs_timeout())
-            self.state = State.DO_NOT_WANT
-            while not self.queue.empty():
-                flag = self.queue.get()
-                flag.set()
+            self.sent_timestamp = self.timestamp
+            message = f'{self.pid} {self.sent_timestamp}'
+            self.state = State.WANTED
+            self.broadcast_and_wait(message)
+            self.possess()
 
     def handle_connection(self, conn):
         pid, t = self.decode_request(conn.recv(1024))
-        if self.state == State.WANTED:
-            if t > self.timestamp:
-                ready = threading.Event()
-                self.queue.put(ready)
-                ready.wait()
-        elif self.state == State.HELD:
+        self.timestamp = max(t, self.timestamp) + 1
+        if self.state == State.HELD or self.state == State.WANTED and self.to_queue(pid, t):
             ready = threading.Event()
             self.queue.put(ready)
             ready.wait()
         conn.sendall(self.ok_response())
+        conn.close()
 
     def start(self):
         _thread.start_new_thread(self.apply_for_cs, ())
         while not self.terminated.is_set():
             conn, _ = self.socket.accept()
             _thread.start_new_thread(self.handle_connection, (conn,))
+        self.socket.close()
 
 
 def init_processes(n: int):
@@ -116,13 +135,13 @@ if __name__ == '__main__':
     while True:
         command = input('$ ').split(' ')
         if command[0] == 'list':
-            p_list = [f'{process.pid}, {process.state.name}' for process in processes]
+            p_list = [f'P{process.pid}, {process.state.name}' for process in processes]
             print(*p_list, sep='\n')
         elif command[0] == 'time-cs':
             TCS = int(command[1])
         elif command[0] == 'time-p':
             TP = int(command[1])
-        elif command[0] == 'exit':
+        elif command[0] in ['exit', 'quit']:
             break
         else:
             print('Unknown command')
